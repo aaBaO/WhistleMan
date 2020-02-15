fe9870fe31a5e4c3ddd99cde10b5472e2ba3fbbe
abao <abao883@gmail.com>
2020-02-15 17:16:49 +0800

    添加迷雾

---
+++ Assets/Scripts/FogOfWar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FogOfWar : MonoBehaviour
+{
+    public Camera fogOfWarCamera;
+    public RawImage fogOfWarMainImage;
+    RenderTexture m_fogOfWarRt;
+
+    public int rtWidth = 1024;
+
+    void Start()
+    {
+        fogOfWarCamera = GetComponent<Camera>();
+        float ratio = Screen.width * 1.0f / Screen.height; 
+        int width = rtWidth;
+        int height = (int)(rtWidth / ratio);
+
+        RenderTextureDescriptor rtd = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32);
+        rtd.depthBufferBits = 0;
+        rtd.useMipMap = false;
+        RenderTexture rt = new RenderTexture(rtd);
+        rt.name = "FogOfWarMain";
+        rt.filterMode = FilterMode.Point;
+        fogOfWarCamera.targetTexture = rt;
+
+        fogOfWarMainImage.texture = rt;
+    }
+
+}
