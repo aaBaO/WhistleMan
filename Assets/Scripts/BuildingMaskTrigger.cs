c7d3d1b5f984bfb488f6ea824b3a3f31d5f95dfe
abao <abao883@gmail.com>
2020-02-16 23:37:40 +0800

    重新绘制第一关场景
    * 添加场景TileAsset
    * 添加角色进入房屋时看到房屋内部

---
+++ Assets/Scripts/BuildingMaskTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMaskTrigger : MonoBehaviour
+{
+
+    GameObject m_spriteMask;
+
+    bool m_isMaskEnable;
+
+    void Start()
+    {
+        m_isMaskEnable = false;
+        m_spriteMask = transform.GetChild(0).gameObject;
+
+        m_spriteMask.SetActive(m_isMaskEnable);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("BuildingMaskTrigger"))
+        {
+            m_isMaskEnable = !m_isMaskEnable;
+            m_spriteMask.SetActive(m_isMaskEnable);
+        }
+    }
+}
